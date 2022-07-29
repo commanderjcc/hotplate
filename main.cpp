80ad4f06635ececaa51b914fe2665d9e35901e73
commanderjcc <jj1355810@gmail.com>
2022-07-29 17:30:34 -0400

    init

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+const int PLATE_SIZE = 5;
+
+void OutputPlate();
+
+int main() {
+    cout << "Hello, World!" << endl;
+    OutputPlate();
+    return 0;
+}
+
+void OutputPlate() {
+    cout << "taadaa";
+}
